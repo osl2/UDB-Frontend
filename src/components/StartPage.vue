cd94ee1b54c398086c7b8d49427865f4c04ff2de
Svenja <svenjafellbaum@aol.com>
2019-07-15 14:55:57 +0200

    bootsrapVue + button to sandbox mode

--- src/components/StartPage.vue
+++ src/components/StartPage.vue
@@ -7,21 +7,26 @@
 
         <div class="container">
             <div class="card-deck mb-3 text-center">
+
                 <div class="card mb-4 box-shadow">
                     <div class="card-header">
                         <h4 class="my-0 font-weight-normal">{{ $t("standbox.titleStartpage") }}</h4>
                     </div>
                     <div class="card-body">
-
                         <ul class="list-unstyled mt-3 mb-4">
                             <li>Ein Datenbank hochladen</li>
                             <li>Queries ausführen</li>
                             <li>etc</li>
                             <li>etc</li>
                         </ul>
-                        <button type="button" class="btn btn-lg btn-block btn-primary">Go</button>
+                        <router-link to="/sandbox">
+                            <button type="button" class="btn btn-lg btn-block btn-primary" >
+                             Go
+                            </button>
+                        </router-link>
                     </div>
                 </div>
+
                 <div class="card mb-4 box-shadow">
                     <div class="card-header">
                         <h4 class="my-0 font-weight-normal">Lehrer Bereich</h4>
@@ -34,9 +39,10 @@
                             <li>etc</li>
                             <li>etc</li>
                         </ul>
-                        <button type="button" class="btn btn-lg btn-block btn-primary">Einloggen</button>
+                        <button type="button" class="btn btn-lg btn-block btn-primary" @click="login">Einloggen</button>
                     </div>
                 </div>
+
                 <div class="card mb-4 box-shadow">
                     <div class="card-header">
                         <h4 class="my-0 font-weight-normal">Kurs beitretten</h4>
