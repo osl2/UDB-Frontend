d581dc4929bbdeb17875b935b3f97b1c639a7dcb
Lisa Weigel <lisa.weigel@online.de>
2019-07-15 17:43:50 +0200

    erster Testversuch Componenten switchen

---
+++ src/components/Test.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h3>
+            HALLO DAS SWITCHEN FUNKTIONIERT
+        </h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Test.vue"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Sandbox.vue
+++ src/views/Sandbox.vue
@@ -1,16 +1,18 @@
 <template>
     <div>
-    <b-container class="bv-example-row">
-        <b-row>
-            <b-col sm="10" offset="1">
-                <Query
-                        @executeQuery="executeQuery"
-                ></Query>
-            </b-col>
-        </b-row>
+        <component :is="dynamicComponent"></component>
+        <b-button @click="switchComponent"> Wechsel </b-button>
+        <b-container class="bv-example-row">
+            <b-row>
+                <b-col sm="10" offset="1">
+                    <Query
+                            @executeQuery="executeQuery"
+                    ></Query>
+                </b-col>
+            </b-row>
 
-        {{query}}
-    </b-container>
+            {{query}}
+        </b-container>
     </div>
 </template>
 
@@ -19,21 +21,34 @@
 
     import Vue  from 'vue';
     import Query from '@/components/Query.vue';
+    import Test from '@/components/Test.vue'
 
     export default Vue.extend({
         components: {
             Query,
+            Test,
         },
         data() {
             return {
+                isPointAndClickActive: false,
+                dynamicComponent: Query,
                 query: '',
             }
         },
         methods: {
             executeQuery: function(query: string){
                 this.query = query;
+            },
+            switchComponent: function () {
+                if (this.isPointAndClickActive) {
+                    this.isPointAndClickActive = false;
+                    this.dynamicComponent = Query;
+                } else {
+                    this.isPointAndClickActive = true;
+                    this.dynamicComponent = Test;
+                }
             }
-        }
+        },
     })
 
 </script>
