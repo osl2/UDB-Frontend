61408dddcd675967249fc0cb6155ac60002ee0ea
Lisa Weigel <lisa.weigel@online.de>
2019-07-17 19:00:51 +0200

    #6 QueryResult Componente, die Tabelle anzeigt, wenn das Ergebnis auf folgende Weise gegeben ist: 1) ein Array aus Strings, das die Spaltennamen beinhaltet; 2) ein Array aus Arrays, wobei jedes Array eine Zeile der Datenbank darstellt

---
+++ src/components/QueryResult.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="table">
+        Hier kommt ein Ergebnis hin.
+        {{queryResultValues}}
+        {{queryResultColumnNames}}
+        <table class="table">
+            <thead>
+            <tr>
+                <th v-for="columnName in queryResultColumnNames"
+                    scope="col">{{columnName}}</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="column in queryResultValues">
+                <td v-for="value in column">{{value}}</td>
+            </tr>
+            </tbody>
+        </table>
+
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue';
+
+    export default Vue.extend({
+        props: ['queryResultColumnNames', 'queryResultValues'],
+    })
+</script>
+
+<style scoped>
+
+.table {
+    border: 1px lightgray solid;
+}
+
+</style>
