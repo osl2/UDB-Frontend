ef94fa89013782bd900bbe3c3a66a681e8cb3e80
Lisa Weigel <lisa.weigel@online.de>
2019-07-21 17:57:27 +0200

    erste Version für die Arbeitsblattliste

--- src/components/WorksheetList.vue
+++ src/components/WorksheetList.vue
@@ -1,14 +1,56 @@
 <template>
-    
+    <div class="list">
+        <div class="scroll">
+            <b-card-group>
+                <b-card
+                        v-for="worksheet in course.worksheets"
+                        bg-variant="light"
+                        class="card"
+                        v-show="worksheet.isOnline"
+                        >
+                    <b-card-title>
+                        {{worksheet.name}}
+                    </b-card-title>
+                    <b-button
+                            slot="footer"
+                            @click="$emit('loadWorksheet', 'love')"
+                    >Blatt bearbeiten
+                    </b-button>
+                    <div class="clear"></div>
+                </b-card>
+            </b-card-group>
+        </div>
+    </div>
 </template>
 
 <script>
     import Vue from 'vue';
     export default Vue.extend({
-
+        props: ['course'],
     })
 </script>
 
 <style scoped>
+    .scroll {
+        border: 1px lightgray solid;
+        background: lightgray;
+        height: 12rem;
+        overflow: auto;
+    }
+
+    .card {
+        text-align: center;
+        min-width: 12rem;
+        min-height: 10rem;
+        max-width: 12rem;
+        max-height: 10rem;
+        margin: 10px 20px 10px 10px;
+
+    }
+
+    .btn {
+        background-color: darkcyan;
+        float: bottom;
+    }
 
 </style>
