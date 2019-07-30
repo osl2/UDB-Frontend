319ec0fa332d3373018097d808582949b81cfdf3
Lisa Weigel <lisa.weigel@online.de>
2019-07-30 18:31:14 +0200

    Tests in der Testkomponente

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -5,24 +5,30 @@
         </h3>
         <div>
             {{apitest}}
+            {{errmsg}}
         </div>
     </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import {Vue, Component, Prop} from 'vue-property-decorator';
+import {GetDatabaseRequest} from "@/api/DefaultApi";
 
-export default Vue.extend({
-    data() {
-        return {
-            apitest: {},
-        };
-    },
-    mounted() {
+export default class Test extends Vue {
+  // Data
+  public apitest: object = {};
+  public request: GetDatabaseRequest = {databaseId: '12445'};
+  public test2: object = {};
+  public errmsg: string = '';
+
+
+  public create() {
         this.$store.getters.api.getDatabases()
             .then((response: {}) => (this.apitest = response));
-    },
-});
+        this.$store.getters.api.getDatabase(this.request).then((response: {}) => (this.test2 = response))
+          .catch((error: any) => (this.errmsg = error.message));
+        }
+    }
 </script>
 
 
