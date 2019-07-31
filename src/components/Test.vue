e45d5120a0a059c446592aa82c1f6a77e65fc4c4
Lisa Weigel <lisa.weigel@online.de>
2019-07-31 11:12:14 +0200

    Class Style decorator auf TEst.vue aufgrund von Fehlern wieder geändert

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -8,27 +8,36 @@
             {{errmsg}}
         </div>
     </div>
+
 </template>
 
+
 <script lang="ts">
-import {Vue, Component, Prop} from 'vue-property-decorator';
+import Vue from 'vue';
 import {GetDatabaseRequest} from "@/api/DefaultApi";
 
-export default class Test extends Vue {
-  // Data
-  public apitest: object = {};
-  public request: GetDatabaseRequest = {databaseId: '12445'};
-  public test2: object = {};
-  public errmsg: string = '';
 
 
-  public mounted() {
-        this.$store.getters.api.getDatabases()
-            .then((response: {}) => (this.apitest = response));
-        this.$store.getters.api.getDatabase(this.request).then((response: {}) => (this.test2 = response))
-          .catch((error: any) => (this.errmsg = error.message));
-        }
-    }
+export default Vue.extend({
+
+  data() {
+
+    return {
+
+      apitest: {},
+      request: {databaseId: '14352392'} as GetDatabaseRequest,
+      test2: {},
+      errmsg: '',
+    };
+
+  },
+
+  mounted() {  this.$store.getters.api.getDatabases()
+    .then((response: {}) => (this.apitest = response));
+               this.$store.getters.api.getDatabase(this.request).then((response: {}) => (this.test2 = response))
+      .catch((error: any) => (this.errmsg = error.message));
+  },
+});
 </script>
 
 
