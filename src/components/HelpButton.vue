ff5b001f8e3f2e06e3d8af083ad69cfba811933b
Svenja <svenjafellbaum@aol.com>
2019-07-31 21:40:45 +0200

    Helpbutton

---
+++ src/components/HelpButton.vue
@@ -0,0 +1,24 @@
+<template>
+    <b-button class="help-btn" v-b-modal.modal-help>
+        <b-img rounded="true" src="../../public/img/questionmark.png"></b-img>
+    </b-button>
+    <b-modal id="modal-help" busy="true" >
+        <p>{{this.helpmsg}}</p>
+    </b-modal>
+</template>
+
+<script lang="ts">
+    import {Component, Prop, Vue} from 'vue-property-decorator';
+    export default class extends Vue{
+        @Prop() private helpmsg!: string;
+    }
+</script>
+
+<style scoped>
+    .help-btn{
+        width: 5px;
+        height: 5px;
+
+    }
+
+</style>
