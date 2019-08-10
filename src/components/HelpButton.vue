39f456b1c2f730a45076bc9117201acc5da78736
Lisa Weigel <lisa.weigel@online.de>
2019-08-10 19:50:12 +0200

    modify HelpButton

--- src/components/HelpButton.vue
+++ src/components/HelpButton.vue
@@ -1,25 +1,39 @@
 <template>
     <div>
         <h2>
-            <b-button v-b-popover.hover="'Hier die Hilfetexte einfügen!'" class="helpBtn bg-info ">
-                <b-bagde pill variant="info">?</b-bagde>
-            </b-button>
+            <b-button id="popoverHelpMsg" class="helpBtn bg-info">?</b-button>
+            <b-popover target="popoverHelpMsg"
+                       triggers="hover focus"
+                       :content="helpmsg">
+            </b-popover>
         </h2>
     </div>
 </template>
 
 <script lang="ts">
-import {Component, Prop, Vue} from 'vue-property-decorator';
+  /**
+   * Component to add a HelpButton to a View.
+   * It appears as a Button with a question mark. On hover over you'll get a popover which shows the {{helpmsg}}.
+   * Add HelpButton to your component and insert a
+   * <HelpButton helpmsg="'YourHelpMessageHere'"><HelpButton>
+   * tag in your template.
+   */
+  import {Component, Prop, Vue} from 'vue-property-decorator';
 
-export default class HelpButton extends Vue {
-    public helpmsg: string = 'test';
-}
+  @Component({})
+  export default class HelpButton extends Vue {
+    @Prop() private helpmsg!: string;
+  }
 </script>
 
 <style scoped>
     .helpBtn{
         border-radius: 25px;
 
+    }
+    .cardStyling {
+        background-color: #17a2b8;
+        color: white;
     }
 
 </style>
